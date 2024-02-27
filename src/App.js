d82f13ebbd3eede6d9b843646a391897fb520222
Pramod <pramodmahajan1411200@gmail.com>
2024-02-27 16:48:35 +0530

    Calender done -first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CalendarPage from "./components/Calendar";
+
+function App() {
+  return (
+    <>
+      <div style={{ height: "50%" }}>
+        <CalendarPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
